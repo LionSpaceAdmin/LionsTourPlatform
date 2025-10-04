7311dc5d7b870790cc2751ba3ff48f38e0297f8d
דניאל חנוכייב <Hanudani@gmail.com>
2025-10-04 22:23:57 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "lionsaitour",
+  "appId": "1:233193120963:web:f16b9d7c5ce2a844cfee76",
+  "apiKey": "AIzaSyDTf84VYx34elxxe2MZxsIYsPXjF8GaT6Q",
+  "authDomain": "lionsaitour.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "233193120963"
+};
